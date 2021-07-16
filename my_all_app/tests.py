23fa4eff2b7b4e751fdafc256269c8d43ab9f2db
morgan <facorat.morgan@gmail.com>
2021-07-16 14:55:27 +0200

    tests

--- my_all_app/tests.py
+++ my_all_app/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import AnonymousUser, User
 
-# Create your tests here.
+
+class ViewsTest(TestCase):
+
+    def test_index_anonymous(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'my_all_app/login.html')
+
+    def test_index_login(self):
+        c = Client()
+        self.user = User.objects.create_user(
+            username='fakkotest', password='Morgan771970@')
+        c.force_login(self.user)
+        response = c.get('/', follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'my_all_app/index.html')
