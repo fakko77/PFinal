6b46abfac799bbc45b0e66edfce292815d19aee2
morgan <facorat.morgan@gmail.com>
2021-09-02 09:15:27 +0200

    test heroku

--- my_all_project/settings.py
+++ my_all_project/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
